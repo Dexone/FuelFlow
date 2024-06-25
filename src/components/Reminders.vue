4a09a8dd325441703b215ee3987ee64ccaabd938
Dexone <dm346@mail.ru>
2024-06-25 12:21:15 +0300

    fix

--- src/components/Reminders.vue
+++ src/components/Reminders.vue
@@ -21,11 +21,11 @@
     </div>
 
     <div class="p-3.5 mt-3 inline-flex" @click="topFunction()">
-        <svg class="w-6 h-6 text-blue-600 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        <!-- <svg class="w-6 h-6 text-blue-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
             width="24" height="24" fill="none" viewBox="0 0 24 24">
             <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M5 7h14M5 12h14M5 17h10" />
-        </svg>
-        <button class="text-blue-600">Еще {{ obsl.length - 1 }}</button>
+        </svg> -->
+        <button class="text-blue-600"> ☰ Еще {{ obsl.length - 1 }}</button>
     </div>
 
     </div>
